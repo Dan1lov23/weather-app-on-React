79987636d8a02f4128e1647aafa32e6293ccc0c8
kitik <svy1dan@gmail.com>
2025-05-13 09:28:47 +0300

    app component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MainPage from '../src/page/main page/MainPage.tsx';
+
+export default function App() {
+    return (
+        <>
+            <div className="App">
+                <MainPage />
+            </div>
+        </>
+    )
+}
